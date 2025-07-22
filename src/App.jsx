e301d155ea6c13de68172c316f1f5bb0a59f6195
Muskan <muskanblp05@gmail.com>
2025-07-22 12:42:41 +0530

    Added the Habit tracker page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Dashboard from './components/Dashboard';
+import HabitTracker from './components/HabitTracker';
 import { TransactionProvider } from './components/TransactionContext';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/habit-tracker" element={<HabitTracker />} />
         </Routes>
       </Router>
     </TransactionProvider>
