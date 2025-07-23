ba13275e240dc0bf6a5fbae7d3a09aadfe2edc36
Tanmay Kalra <151675979+TanmayKalra09@users.noreply.github.com>
2025-07-23 16:10:27 +0530

    Revert "Feature/my feature"

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Dashboard from './components/Dashboard';
-import HabitTracker from './components/HabitTracker';
 import { TransactionProvider } from './components/TransactionContext';
 import { CurrencyProvider } from "./components/CurrencyContext";
 
 export default function App() {
   return (
     <TransactionProvider>
-
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/habit-tracker" element={<HabitTracker />} />
-        </Routes>
-      </Router>
-
       <CurrencyProvider>
         <Router>
           <Routes>
@@ -25,7 +15,6 @@ export default function App() {
           </Routes>
         </Router>
       </CurrencyProvider>
-
     </TransactionProvider>
   );
 }
